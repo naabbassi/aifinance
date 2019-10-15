a0cba37cd5bb1688c31606432e9ecdf9e6f34476
Nasser Abbassi <abbasiamanj@gmail.com>
2019-10-15 16:00:42 +0200

    config prermission for heroku

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     */
     
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('pgsql'),
     /*
     |--------------------------------------------------------------------------
     | Database Connections
